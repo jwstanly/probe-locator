8dea4fb234e7e9a5134178cdacdcb9dfbb7148e8
jwstanly <jwstanly@yahoo.com>
2022-03-13 23:00:24 -0400

    dev env setup

---
+++ config.example.ts
@@ -0,0 +1,11 @@
+import { Config } from './types';
+
+const config: Config = {
+  wifiInterface: 'en0',
+  latRange: [30.5, 31.5],
+  longRange: [80.2, 81.2],
+  city: 'Atlanta',
+  encodedToken: 'TOKEN_HERE',
+};
+
+export default config;
